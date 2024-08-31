1c135bcdb447f2da59f73e13eecb1120f52ddca7
vachegunashvili <ananoobarabadze@gmail.com>
2024-08-31 14:28:20 +0400

    commit

---
+++ day22/homework/homework.py
@@ -0,0 +1,18 @@
+for i in range(1,11):
+    print(i)
+
+for i in range(1,16):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,101,2):
+    print(i)
+
+
+num1 = int(input("enter ur number : "))
+while num1 <100 :
+    print(num1)
+    num1 += num1
+    
