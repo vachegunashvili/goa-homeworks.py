c1797efb23e36615d83b739c1cab7ce7b45c2c67
vachegunashvili <ananoobarabadze@gmail.com>
2024-09-01 14:41:22 +0400

    commit

--- day22/homework/homework.py
+++ day22/homework/homework.py
@@ -15,4 +15,40 @@ num1 = int(input("enter ur number : "))
 while num1 <100 :
     print(num1)
     num1 += num1
+num = 1 
+while num <= 100:
+    print(num)
+    num += 1
+
+num = 1
+while num1 <= 10 :
+    print(num)
+    num += 1
+
+leap_year =int(input("enter any number : "))
+
+if leap_year ==4 :
+    print("its a leap year")
+else :
+    print("its a classic year")
+
+    
+print("what is 4+4")
+answer = int(input(" user answer : "))
+if answer == 8 :
+    print("correct")
+else : 
+    print("incorrect")
+
+height = int(input("enter your height : "))
+weight = int(input("enter your weight : "))
+if height and weight > 200 :
+    print("you are too fat to go to this attraction")
+else : 
+    print("you can go to this attraction")
+
+
+
+
+
     
