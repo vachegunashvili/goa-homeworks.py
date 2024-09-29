4e897763688ccd84f9fc549a66fa8ab09469fd16
vachegunashvili <161934989+vachegunashvili@users.noreply.github.com>
2024-09-29 15:18:00 +0400

    Add files via upload

---
+++ day24/homework/hw.py
@@ -0,0 +1,31 @@
+#def g_a (num1):
+   # i = input("please eneter a number 1-100")
+  #  if i => 100:
+      #  print("try again")
+     #  print("try again")
+       # while i != guess:
+       #    print("try again")
+
+#for number in range(1-100):
+    #if number % 3 == 0 and number % 5 ==0:
+        #print("fizzbuzz")
+   # elif number % 3 == 0:
+       # print("fizz")
+   # elif number % 5 == 0:
+    #    print("buzz")
+   # else:
+     #   print("number")
+
+
+
+def len():
+    age = int(input("please enetr ur age"))
+    expiriance = int(input("please enter ur expiriance "))
+    if age >= 18 and expiriance >= 1:
+        print("u have premission to have drivers license")
+    elif age < 18:
+       print("u have to be 18+ to have drivers licanse")
+    else:
+        print("ur expiriance has to be over 1year to have drivers licanse")
+len()
+        
